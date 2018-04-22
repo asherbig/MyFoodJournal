0affd72f8b50a0d6a77343e6f92c9b0bd84ce665
zguthrie3 <zguthrie3@gatech.edu>
2018-04-22 17:50:14 -0400

    Fixes for Filter Functionality
    Fixed issues in Filter Functionality causing errors

--- app/src/main/java/team7202/myfoodjournal/MyReviewsFragment.java
+++ app/src/main/java/team7202/myfoodjournal/MyReviewsFragment.java
@@ -274,9 +274,10 @@ public class MyReviewsFragment extends Fragment implements View.OnClickListener 
                 datum.put("Description", (String) reviewInfo.get("description"));
                 datum.put("Rating", reviewInfo.get("rating") + "/5");
                 datum.put("Date Submitted", (String) reviewInfo.get("date_submitted"));
-                datum.put("UserId", (String) reviewInfo.get("userId"));
+                datum.put("User ID", (String) reviewInfo.get("userId"));
                 datum.put("Review ID", (String) reviewInfo.get("reviewId"));
                 datum.put("Restaurant ID", (String) reviewInfo.get("restaurant_id"));
+                datum.put("Address", (String) reviewInfo.get("address"));
                 data.add(datum);
             }
         }
